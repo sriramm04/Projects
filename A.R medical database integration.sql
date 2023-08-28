ac195fabf706dccf3ba55f5e8abc9f8a124918c7
Sriram V <129077845+sriramm04@users.noreply.github.com>
2023-08-28 15:02:47 +0530

    Update A.R medical database integration.sql

--- A.R medical database integration.sql
+++ A.R medical database integration.sql
@@ -8,12 +8,3 @@ select * from peak_hours;
 select * from monthly_sales order by date(Month_year);
 show tables;
 select * from customer_table;
-
-![Screenshot 2023-08-28 145029](https://github.com/sriramm04/Projects/assets/129077845/cf6f9436-a09a-4bb4-adea-cd655f5589ae)
-![Screenshot 2023-08-28 145226](https://github.com/sriramm04/Projects/assets/129077845/1b7f075e-7b94-49c0-8bcc-dc1a4ce51b72)
-![Screenshot 2023-08-28 145317](https://github.com/sriramm04/Projects/assets/129077845/d311c592-a404-4136-9299-f08d9258cae1)
-![Screenshot 2023-08-28 145350](https://github.com/sriramm04/Projects/assets/129077845/8f74b0b2-543a-4702-a5f5-f8c904b116cb)
-![Screenshot 2023-08-28 145434](https://github.com/sriramm04/Projects/assets/129077845/da1e43f4-2611-4cf9-851d-63862a67115b)
-![Screenshot 2023-08-28 145453](https://github.com/sriramm04/Projects/assets/129077845/62bfe6e0-6dc8-422b-acb8-f81910695c1b)
-![Screenshot 2023-08-28 145544](https://github.com/sriramm04/Projects/assets/129077845/fc25d5cd-3545-4854-b793-7968a5d45475)
-![Screenshot 2023-08-28 145630](https://github.com/sriramm04/Projects/assets/129077845/66b9721f-a520-4059-95e0-e227b557b862)
